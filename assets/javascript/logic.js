24c41fd1c6943b27ac80fd4b807dd7d81432d1aa
rstowe1 <ryanwstowe@gmail.com>
2018-03-25 19:48:40 -0400

    im so proud of myself!

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,6 +1,8 @@
 // these are the buttons that will be added by default
-$(document).ready();
-gifSearch = ['trending', 'funny', 'ouch', 'dumb dogs', 'cats', 'dogs', 'Bassett Hound'];
+$(document).ready(function () {
+
+
+let gifSearch = ['trending', 'funny', 'ouch', 'dumb dogs', 'cats', 'dogs', 'Bassett Hound'];
 
 $('#search-input').on('click focusin', function () {
   this.value = '';
@@ -11,7 +13,9 @@ $('#newGif').on('click',function()  {
   var newSearch = $('#search-input').val().trim();
   event.preventDefault();
   gifSearch.push(newSearch);
-  $('#search-input').val("")
+  $('#search-input').val("");
+
+ return mkButtons();
 });
 
 // Removes last value in the array - to be displayed on the side
@@ -19,55 +23,27 @@ $('#nixGif').on('click',function()  {
   var remSearch = $('#search-input').val().trim();
   event.preventDefault();
  gifSearch.splice($.inArray(remSearch,gifSearch),1);
-  $('#search-input').val("")
+  $('#search-input').val("");
+
+ return mkButtons()
 });
 
-function displayButton() {
+function mkButtons() {
+
+  $('#giphySearches').empty();
   for (var i = 0; i < gifSearch.length; i++) {
-    var dispButton = $(':button');
-    dispButton.addClass('FML');
+    var dispButton = $('<button>');
+    dispButton.addClass('giphy');
     dispButton.addClass('btn btn-primary');
-    dispButton.attr('name', gifSearch[i]);
+    dispButton.attr('data-name', gifSearch[i]);
     dispButton.text(gifSearch[i]);
     $('#giphySearches').append(dispButton);
+
   }
-
 }
+  return mkButtons();
+});
 
 
 
 
-
-  // function displayButtons() {
-  //   $('#giphySearches').empty();
-  //
-  //   for (var i = 0; i < actions.length; i++) {
-  //     var dispButton = $(':button');
-  //     dispButton.addclass('FML');
-  //     dispButton.addclass('btn btn-primary');
-  //     dispButton.attr('name', defaultSearch [i]);
-  //     dispButton.text(actions[i]);
-  //     $('#giphySearches').append(dispButton);
-  //   }
-  // }
-  //
-  // // Add a new search
-  // function newButton() {
-  //   $('#newGif').on('click', function () {
-  //     var newSearch = $('#search-input').val().trim();
-  //
-  //   });
-  //
-  //   defaultSearch.push(newSearch)
-  // }
-  //
-  // function remButton() {
-  //   $('#nixGif').on('click', function() {
-  //     var newSearch = $('#search-input').val().trim();
-  //
-  //     gifSearch.push(newSearch)
-  //
-  //   });
-  //
-  // }
-// });
